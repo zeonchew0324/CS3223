b57e13bc36cd2e05c7d074ed6da052548afe6aee
Chew Tsze Honn <zeonchew0324@gmail.com>
2026-08-30 15:18:50 +0800

    fix compiling error

--- SimpleDBEngine/src/simpledb/plan/PlanTest1.java
+++ SimpleDBEngine/src/simpledb/plan/PlanTest1.java
@@ -9,9 +9,7 @@ public class PlanTest1 {
       SimpleDB db = new SimpleDB("studentdb");
       Transaction tx = db.newTx();
 
-      String qry = "select sid, sname, gradyear from student";
-      // later, for part 2:
-      // String qry = "select sid, sname, gradyear from student order by gradyear asc, sname desc";
+      String qry = "select sid, sname, gradyear from student order by gradyear asc, sname desc";
 
       Plan p = db.planner().createQueryPlan(qry, tx);
       Scan s = p.open();
